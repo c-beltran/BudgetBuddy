6cb68b29903962e48f771c63470f7fd5685dd6b0
c-beltran <carlosbeltran90@gmail.com>
2018-11-27 13:38:34 -0500

    initialized app

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const mysql = require('mysql');
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mysql = require('mysql');
 
-const connection = mysql.createConnection({
+//create a connectio with DB
+var db = mysql.createConnection({
 	host     : 'localhost',
-  	user     : 'userRoot',
-  	password : 'password',
+  	user     : 'root',
+  	password : '',
   	// database : 'my_db'
 });
 
-connection.connect();
+//connect
+db.connect(function(err){
+	if(err){
+		console.log(err);
+	}else{
+		console.log("mysql db is connected");
+	}
+});
 
+//route to create db table
+// app.get('/createdb', function(req, res){
+// 	var sqlDatabase = 'CREATE DATABE budgetApp';
+// 	db.query(sqlDatabase, function(err, result){
+// 		if(err){
+// 			console.log(err);
+// 		}else{
+// 			console.log(result); //this prints the db structure
+// 			res.send('Database Created Successfully!'); //you should see this message in your local browser localhost/3000/createdb
+// 		}
+// 	});
+
+// });
 
 app.listen(3000, function() {
 	console.log("SERVER LISTENING ON PORT 3000 ... ");
