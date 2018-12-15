468f1385dec47f1cfe94c722feb23f642bc52cc0
c-beltran <carlosbeltran90@gmail.com>
2018-12-15 12:53:24 -0500

    added dropdown to main user page, added todays date

--- public/js/home.js
+++ public/js/home.js
@@ -12,3 +12,22 @@
   evt.currentTarget.className += " active";
 }
 document.getElementById("defaultOpen").click();
+
+
+  var loc = document.getElementById("show-date");
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+    dd = '0'+dd
+  } 
+
+  if(mm<10) {
+    mm = '0'+mm
+  } 
+
+  today = mm + '/' + dd + '/' + yyyy;
+
+  loc.innerHTML = today;
